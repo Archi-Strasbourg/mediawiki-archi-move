78603f9c515512ee754a2f9df059b6ce2db3282b
Pierre Rudloff <contact@rudloff.pro>
2017-11-08 01:15:09 +0100

    Initial commit

---
+++ ArchiMove.php
@@ -0,0 +1,19 @@
+<?php
+namespace ArchiMove;
+
+class ArchiMove
+{
+    public static function onTitleMove($oldTitle, $newTitle)
+    {
+        if ($oldTitle->getNamespace() == NS_ADDRESS && $newTitle->getNamespace() == NS_ADDRESS) {
+            $oldNewsTitle = \Title::newFromText($oldTitle->getText(), NS_ADDRESS_NEWS);
+            $newNewsTitle = \Title::newFromText($newTitle->getText(), NS_ADDRESS_NEWS);
+
+            $oldNewsTitle->moveTo(
+                $newNewsTitle,
+                true,
+                'Page d\'actualités renommée automatiquement suite à un renommage de la page principale'
+            );
+        }
+    }
+}
