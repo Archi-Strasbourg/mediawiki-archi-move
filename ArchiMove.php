b96aafefb30340bbc6582a896c66132dbf9958aa
Pierre Rudloff <prudloff@insite.coop>
2021-08-26 10:39:13 +0200

    La méthode moveTo n'existe plus (Insite #26862)

--- ArchiMove.php
+++ ArchiMove.php
@@ -2,19 +2,22 @@
 
 namespace ArchiMove;
 
+use MovePage;
+use Title;
+
 class ArchiMove
 {
     public static function onTitleMove($oldTitle, $newTitle)
     {
         if ($oldTitle->getNamespace() == NS_ADDRESS && $newTitle->getNamespace() == NS_ADDRESS) {
-            $oldNewsTitle = \Title::newFromText($oldTitle->getText(), NS_ADDRESS_NEWS);
-            $newNewsTitle = \Title::newFromText($newTitle->getText(), NS_ADDRESS_NEWS);
+            global $wgUser;
 
-            $oldNewsTitle->moveTo(
-                $newNewsTitle,
-                true,
-                'Page d\'actualités renommée automatiquement suite à un renommage de la page principale'
+            $movePage = new MovePage(
+                Title::newFromText($oldTitle->getText(), NS_ADDRESS_NEWS),
+                Title::newFromText($newTitle->getText(), NS_ADDRESS_NEWS)
             );
+
+            $movePage->move($wgUser, 'Page d\'actualités renommée automatiquement suite à un renommage de la page principale');
         }
     }
 }
